34ac59c6a6222c7e49201e817a3ec5da7d0b2a64
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-05-03 22:57:33 +0400

    Update helper_func.c

--- helper_func.c
+++ helper_func.c
@@ -1,55 +1,125 @@
 #include "main.h"
-#define TOKEN_DELIM " \t\r\n\a"
+
+/*char *fullpath;*/
+
 /**
- * formatln - format line
- * @ln: line
- * Return: token array
+ * printerror - prints error
+ * @command: command inserted
  */
-char **formatln(char *ln)
+
+void printerror(char *const command[])
 {
-	int bufsize = 64, i = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
+	fprintf(stderr, "./hsh: 1: %s: not found\n", command[0]);
+	free(*command);
+	exit(127);
+}
 
-	if (tokens == NULL)
+/**
+ * _getenv - get environment variable
+ * @name: name of environment variable
+ * @envp: pointer to environment variables
+ * Return: pointer to environment variable
+ */
+
+char *_getenv(const char *name, char **envp)
+{
+	char **env, *separator;
+
+	for (env = envp; *env != NULL; env++)
 	{
-		perror("failed to allocate tokens\n");
-		return (NULL);
-	}
-
-	token = strtok(ln, TOKEN_DELIM);
-	while (token != NULL)
-	{
-		tokens[i] = token;
-		i++;
-
-		if (i >= bufsize)
+		separator = strchr(*env, '=');
+		if (separator != NULL)
 		{
-			bufsize += 64;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (tokens == NULL)
+			if (strncmp(*env, name, separator - *env) == 0)
 			{
-				perror("failed to reallocate tokens\n");
-				return (NULL);
+				return (separator + 1);
 			}
 		}
-
-		token = strtok(NULL, TOKEN_DELIM);
 	}
-	tokens[i] = NULL;
-	return (tokens);
+	return (NULL);
 }
+
 /**
- * getln - getline
- * Return: ln
+ * pathfinder - finds the correct path for given alias
+ * @command: given input to check for path
+ * @cmd: argument
+ * @envp: pointer to environment variables
+ * Return: pointer to the string array or NULL if failed
  */
-char *getln(void)
+
+char **pathfinder(char *cmd, char **command, char **envp)
 {
+	char *current_path, *temp_path;
+	char *path_tok;
+	size_t arglen = strlen(cmd);
 
-	char *ln = NULL;
-	unsigned long bufsize = 0;
+	fullpath = NULL;
 
-	getline(&ln, &bufsize, stdin);
+	if (strchr(cmd, '/') != NULL && access(cmd, F_OK) == 0)
+	{
+		command[0] = cmd;
+		return (command);
+	}
 
-	return (ln);
+	path_tok = NULL;
+	current_path = _getenv("PATH", envp);
+	temp_path = strdup(current_path);
+	path_tok = strtok(temp_path, ":");
+
+	while (path_tok)
+	{
+		fullpath = malloc(arglen + strlen(path_tok) + 2);
+		sprintf(fullpath, "%s/%s", path_tok, cmd);
+		if (access(fullpath, F_OK) == 0)
+		{
+			command[0] = fullpath;
+			free(temp_path);
+			return (command);
+		}
+		path_tok = strtok(NULL, ":");
+		free(fullpath);
+	}
+	free(temp_path);
+	return (NULL);
+}
+
+/**
+ * execute - function to execute commands
+ * @command: input froom user
+ * @envp: enviroment path
+ * Return: -1 if failed and 0 if success
+ */
+
+int execute(char *const command[], char **envp)
+{
+	pid_t id;
+	int status, i;
+	char **temp = pathfinder(command[0], (char **) command, envp);
+
+	if (temp != NULL)
+	{
+		id = fork();
+		if (id < 0)
+		{
+			perror("fork failed");
+			return (-1);
+		} else if (id == 0)
+		{
+			if (_getenv("PATH", envp) == NULL && access(command[0], F_OK) != 0)
+				printerror(command);
+			execve(temp[0], command, envp);
+			for (i = 0; command[i] != NULL; i++)
+				free(command[i]);
+			exit(EXIT_FAILURE);
+		}
+		wait(&status);
+		free(fullpath);
+	} else
+		printerror(command);
+	if (errno == -1)
+	{
+		free(*command);
+		exit(2);
+	}
+	return (0);
 }
